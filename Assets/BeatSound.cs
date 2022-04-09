040df67b4aaf017192cd0b166131645413b0d7d9
IvanPavlov <ivanpavlov7007@gmail.com>
2022-04-09 20:18:46 +0200

    Create heart beat demo

---
+++ Assets/BeatSound.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeatSound : MonoBehaviour
+{
+    AudioSource aud;
+    void Start()
+    {
+        aud = GetComponent<AudioSource>();
+    }
+
+    public void PlayBeat()
+    {
+        aud.Stop();
+        aud.PlayOneShot(aud.clip);
+    }
+}
