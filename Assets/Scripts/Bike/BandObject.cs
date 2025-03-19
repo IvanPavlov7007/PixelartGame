212d94ea1d2ed06f8c9ef95bf47ccb57386ee595
IvanPavlov <ivanpavlov7007@gmail.com>
2025-03-19 09:00:31 +0100

    Create SideBandObjects

---
+++ Assets/Scripts/Bike/BandObject.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BandObject : MonoBehaviour
+{
+    public virtual float bandPosition { get; set; }
+    public virtual float boundary { get; set; }
+
+    public virtual void Start()
+    {
+        WorldBand.Instance.registerBandObject(this);
+    }
+
+    protected virtual void OnDestroy()
+    {
+        WorldBand.Instance.removeBandObject(this);
+    }
+
+    public virtual void UpdateWorldPosition(float currentPosition)
+    {
+    }
+}
+
+[System.Serializable]
+public struct ScreenContext
+{
+    public Transform center;
+    public Rect worldRect;
+}
---
+++ Assets/Scripts/Bike/SideViewBandObject.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SideViewBandObject : BandObject
+{
+    [Tooltip("Values bigger than 1 slow down the object(put onto background) \nvalues less than 1 speed up (put onto foreground) \n1 means the object renders at the camera's plane ")]
+    public float paralaxRange = 1f;
+    public float xBandPosition;
+    public float xBoundary = 20f;
+    public override float bandPosition { get => xBandPosition; set => xBandPosition = value; }
+    public override float boundary { get => xBoundary; set => xBoundary = value; }
+    public override void UpdateWorldPosition(float currentPosition)
+    {
+        //translating into camera coordinates
+        var delta = paralaxPosition(currentPosition);
+        if (Mathf.Abs(delta) <= xBoundary)
+        {
+            setWorldPos(delta);
+        }
+        else
+        {
+            //hide
+            setWorldPos(2 * boundary);
+        }
+    }
+
+    /// <summary>
+    /// distant moving objects like clouds or mountains move on a further plane, which is compressed relative to this one
+    /// or opposite for the foreground objects
+    /// </summary>
+    /// <returns></returns>
+    protected virtual float paralaxPosition(float currentPosition)
+    {
+        //if paralaxRange = 1 equation becomes  return xBandPosition - currentPosition
+        return (xBandPosition - currentPosition) / paralaxRange;
+    }
+
+    protected virtual void setWorldPos(float xPos)
+    {
+        var pos = transform.position;
+        pos.x = xPos;
+        transform.position = pos;
+    }
+}
