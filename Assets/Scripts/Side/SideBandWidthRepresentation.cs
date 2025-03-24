9f8ea0c8bf083f25b208b05f26e0eb6aeb927557
IvanPavlov <ivanpavlov7007@gmail.com>
2025-03-24 19:26:16 +0100

    Side bike leaning animation

---
+++ Assets/Scripts/Side/SideBandWidthRepresentation.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SideBandWidthRepresentation : SideRepresentation
+{
+    public float minY = -0.2f;
+    public float maxY = 0f;
+    public float minBandWidth = -1f;
+    public float maxBandWidth = 1f;
+
+    float initY;
+    private void Start()
+    {
+        initY = transform.localPosition.y;
+
+    }
+    public override void Draw(float bandPosition)
+    {
+        //translating into camera coordinates
+        var delta = parallaxPosition(bandPosition);
+        var sideY = calculateCompressedBandPosition();
+
+        if (Mathf.Abs(delta) <= xBoundary)
+        {
+            setWorldPos(delta, sideY);
+        }
+        else
+        {
+            //hide
+            setWorldPos(2 * xBoundary, sideY);
+        }
+    }
+
+    protected virtual float calculateCompressedBandPosition()
+    {
+        float t = Mathf.InverseLerp(minBandWidth,maxBandWidth, bandObject.bandWidthPosition);
+        return initY + Mathf.Lerp(minY, maxY, t);
+    }
+
+    protected virtual void setWorldPos(float xPos, float yPos)
+    {
+
+
+        var pos = transform.localPosition;
+        pos.x = xPos;
+        pos.y = yPos;
+        transform.localPosition = pos;
+    }
+}
---
+++ Assets/Scripts/Side/SideBikeVisualController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SideBikeVisualController : MonoBehaviour
+{
+
+    [SerializeField]
+    BikeControl bikeControl;
+    [SerializeField]
+    public Sprite leaningLeft, leaninghRight;
+    SpriteRenderer sr;
+
+    [Range(0f, 1f)]
+    public float tolerance = 0.2f;
+
+    Sprite origSprite;
+    private void Start()
+    {
+        sr = GetComponentInChildren<SpriteRenderer>();
+        origSprite = sr.sprite;
+    }
+
+    void Update()
+    {
+        var dir = bikeControl.direction.x;
+        if (dir > tolerance)
+        {
+            sr.sprite = leaninghRight;
+        }
+        else if (dir < -tolerance)
+        {
+            sr.sprite = leaningLeft;
+        }
+        else
+        {
+        { 
+            sr.sprite = origSprite;
+        }
+        }
+    }
+
+}
